c52804dbd75b3bcd54384d22dff64e50bc30a1d2
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2025-09-27 20:15:42 +0400

    refactor: added request model validation for reservations
    Signed-off-by: Abhiroop Santra <abhiroop43@users.noreply.github.com>

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,17 @@
+import { IsISO8601, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateReservationDto {
+  @IsISO8601()
   startDate: Date;
+
+  @IsISO8601()
   endDate: Date;
+
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
+
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
